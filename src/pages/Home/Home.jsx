27e61a5dc9fa16abf3dee4d4271360b209056bba
Shivansh <shivansh.sd8@gmail.com>
2025-04-13 08:05:28 +0530

    improving routing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,15 +4,10 @@ import Posts from '../../components/Posts';
 
 export default function Home() {
   return (
-    // <div className="flex flex-col md:flex-row gap-6 px-4 md:px-12 mt-8 h-[calc(100vh-64px)] overflow-hidden">
-    // <div className="flex flex-col md:flex-row gap-6 px-4 md:px-12 mt-8 md:h-[calc(100vh-64px)]">
     <div className="flex flex-col md:flex-row gap-6 px-4 md:px-12 mt-8 md:h-[calc(100vh-64px)] md:overflow-hidden">
-
       {/* Left Sidebar */}
       <div className="md:w-1/3 w-full space-y-6 md:sticky md:top-20 relative">
-
         <ProfileCard />
-
         {/* Top Topics */}
         <div className="bg-white rounded-lg p-4 shadow-md">
           <h2 className="text-lg font-semibold mb-2">Top Topics</h2>
@@ -27,10 +22,7 @@ export default function Home() {
       </div>
 
       {/* Right/Main Content */}
-      {/* <div className="md:w-2/3 w-full flex flex-col gap-6 overflow-y-auto pr-2 h-[calc(100vh-64px)]"> */}
       <div className="md:w-2/3 w-full flex flex-col gap-6 md:overflow-y-auto pr-2 md:h-[calc(100vh-64px)]">
-
-
         {/* Create Post */}
         <div className="-mx-4 md:mx-0">
           <CreatePost />
